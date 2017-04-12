a1d744c69b0a4a18f783820b7d1d1b79ea6e56b0
formation <formation@FORMATI-S1URF2D>
2017-04-12 16:47:29 +0200

    user

--- Wiki/src/com/formation/wikiDao/UserDao.java
+++ Wiki/src/com/formation/wikiDao/UserDao.java
@@ -1,7 +1,7 @@
 package com.formation.wikiDao;
 
 public interface UserDao {
-	public void addUser(User u);
-	public void UpdateUser(User u);
+//	public void addUser(User u);
+	//public void UpdateUser(User u);
 	public void getUser(int id);
 }
